3ec50f25571d8abe8584a479275f6fe4f0faf07e
Błażej Bartoszewski <blazejbart@gmail.com>
2023-09-28 14:05:30 +0200

    task2

--- src (2)/src/js/task-02.js
+++ src (2)/src/js/task-02.js
@@ -1,8 +1,26 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+// Znajdź listę ul#ingredients na stronie.
+const ingredientsList = document.getElementById("ingredients");
+
+// Iteruj przez tablicę ingredients i twórz <li> dla każdego składnika.
+ingredients.forEach((ingredient) => {
+  // Utwórz nowy element <li>.
+  const listItem = document.createElement("li");
+
+  // Dodaj nazwę składnika jako jego zawartość tekstową.
+  listItem.textContent = ingredient;
+
+  // Dodaj klasę "item" do elementu <li>.
+  listItem.classList.add("item");
+
+  // Dodaj utworzony element <li> do listy ul#ingredients.
+  ingredientsList.appendChild(listItem);
+});
