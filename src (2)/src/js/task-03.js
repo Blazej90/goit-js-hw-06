a8e78821a5a1ddf9b0a0e1d5628dde8bbe965e5a
Błażej Bartoszewski <blazejbart@gmail.com>
2023-10-01 21:09:39 +0200

    task 3

--- src (2)/src/js/task-03.js
+++ src (2)/src/js/task-03.js
@@ -1,14 +1,22 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+// Znajdź listę ul.gallery na stronie.
+const galleryList = document.querySelector(".gallery");
+
+// Przejdź przez tablicę images i dodaj elementy do listy galerii.
+images.forEach((image) => {
+  const imageElement = `<img src="${image.url}" alt="${image.alt}">`;
+  galleryList.insertAdjacentHTML("beforeend", `<li>${imageElement}</li>`);
+});
